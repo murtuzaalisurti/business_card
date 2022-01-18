e449db3dccfcfdf0f67b433fcaa5a1697c917935
murtuza <murtuza.creativity@gmail.com>
2022-01-18 12:23:24 +0530

    modified download code and used a new library

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import Card from "./components/Card";
 import {UserInputWrap, Input, Textarea, Label, Button} from './styled/UserInputSection'
 import {HeadingStyled} from './styled/Headings'
 import {useState} from 'react'
-import html2canvas from 'html2canvas'
+import * as htmlToImage from 'html-to-image'
+import download from 'downloadjs'
 
 function App() {
 
@@ -15,31 +16,17 @@ function App() {
   const [image, setImage] = useState();
   const [color, setColor] = useState();
 
-  console.log(document.querySelector("#image"))
   if(document.querySelector("#image") !== null){
     if(document.querySelector("#image").files.length === 1){
       document.querySelector("#upload_label").innerHTML = "Uploaded Successfully";
     }
   }
 
-  function download(){
-    html2canvas(document.querySelector("#card"), {
-      allowTaint: true,
-      backgroundColor: "rgba(255,255,255,0)",
-      scrollX: -window.scrollX,
-      scrollY: -window.scrollY,
-      windowWidth: document.documentElement.offsetWidth,
-      windowHeight: document.documentElement.offsetHeight,
-      scale: 10
-    }).then((canvas) => {
-      console.log(document.documentElement.offsetHeight, window.scrollX, window.scrollY)
-      let link = document.createElement("a");
-      link.className = "download-link";
-      link.setAttribute("download", "business_card");
-      link.setAttribute("href", canvas.toDataURL('image/png'));
-      link.style = "display: none;"
-      document.body.appendChild(link);
-      link.click();
+  function download_image(){
+    htmlToImage.toPng(document.querySelector("#card"), {
+      quality: 1.0
+    }).then((dataUrl) => {
+      download(dataUrl, 'business_card_image')
     })
   }
 
@@ -55,7 +42,7 @@ function App() {
         <Input type="email" onChange={(e) => {setEmail(e.target.value)}} value={email} id="email" placeholder="Email" required autoComplete="off" />
         <Textarea type="text" onChange={(e) => {setAbout(e.target.value)}} value={about} id="about" placeholder="A little bit about you.." rows="5" required autoComplete="off" />
         <Textarea type="text" onChange={(e) => {setServices(e.target.value)}} value={services} id="interests" placeholder="Services offered..." rows="5" required autoComplete="off" />
-        <Button onClick={() => {download()}}>Download</Button>
+        <Button onClick={() => {download_image()}}>Download</Button>
         <Input type="color" onChange={(e) => {setColor(e.target.value)}} value={color} id="color" placeholder="choose color" style={{display: 'none'}} />
       </UserInputWrap>
       <Card name={name} occupation={occupation} website={website} email={email} linkedin about={about} services={services} github twitter instagram color={color} image_src={URL.createObjectURL(new Blob([image], {type: "image"}))} />
